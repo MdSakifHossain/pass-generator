ed2f18f383c34408b51f2ac88b2fc02adcc27a80
MdSakifHossain <sakif015sakif@gmail.com>
2024-03-17 22:11:58 +0600

    update: made it into a PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const updateSW = registerSW({
+	onNeedRefresh() {
+		if (confirm("New content available. Wanna Reload?")) {
+			updateSW(true);
+		}
+	},
+	onOfflineReady() {
+		if (
+			confirm(
+				'App is offline ready.\nYou can install it by clicking on the "Install app" option on the 3 dot menu at the top right corner.'
+			)
+		) {
+		}
+	}
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			manifest: {
+				name: "Password Generator",
+				short_name: "Pass Generator",
+				description: "Teri maa ki chut... 💀💀😘😘",
+				scope: "/",
+				theme_color: "#f59e0b",
+				background_color: "#f59e0b",
+				icons: [
+					{
+						src: "/icon.svg",
+						sizes: "192x192",
+						type: "image/svg+xml"
+					},
+					{
+						src: "/icon.svg",
+						sizes: "512x512",
+						type: "image/svg+xml"
+					},
+					{
+						src: "/icon.svg",
+						sizes: "any",
+						type: "image/svg+xml",
+						purpose: "any"
+					}
+				]
+			},
+			registerType: "prompt",
+			workbox: {
+				clientsClaim: true,
+				skipWaiting: true,
+				cleanupOutdatedCaches: true,
+				globPatterns: ["**/*.{js,css,html,ico,png,svg}"]
+			},
+			includeAssets: ["**/*"],
+			devOptions: {
+				enabled: true
+			}
+		})
+	]
+});
